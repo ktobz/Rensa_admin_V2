9feee44ffcffd66e72ea66fc52a2dcc94adae4c6
Simeon Kenneth T <51843241+KenRelic@users.noreply.github.com>
2024-09-14 03:18:01 +0100

    feat: updates profileUpdate request data

--- src/modules/users/components/UpdateUserForm.tsx
+++ src/modules/users/components/UpdateUserForm.tsx
@@ -29,7 +29,7 @@ export const UpdateUserForm = ({
   const initialData = {
     firstName: initData?.firstName || "",
     lastName: initData?.lastName || "",
-    userName: initData?.userName || "",
+    userId: initData?.id || "",
   };
 
   const [isSaving, setIsSaving] = React.useState(false);
@@ -41,7 +41,7 @@ export const UpdateUserForm = ({
       const formData = new FormData();
       formData.append('FirstName',formValues.firstName );
       formData.append('LastName',formValues.lastName );
-      formData.append('Username',formValues.userName );
+      formData.append('UserId',formValues.userId );
       
       const { data } = await CustomerService.updateProfile(formData);
 
